bf7f85db06b0db0684cb8555a3e6dbf95ef09d3a
Elvis O <elvis.osujic@gmail.com>
2023-12-05 06:47:04 +0100

    added sepolia and rainbowkit to app.ts

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@rainbow-me/rainbowkit/styles.css";
+import "@/styles/globals.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { AppProps } from "next/app";
+
+const { chains, publicClient } = configureChains([sepolia], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "onChain-BigDev-Dao",
+  projectId: "7c4eeedc5ea1346fa6b17f19937733da",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
